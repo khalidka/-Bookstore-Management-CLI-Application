7d4ce19e4e4ea936db014dea49c9301bae942219
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-20 00:22:24 +0300

    feat:set Book class

--- bookstoreClass.py
+++ bookstoreClass.py
@@ -3,6 +3,10 @@ class Author:
         self.name = name
 
 
+class Book:
+    def __init__(self, title, publication_year, author_id):
+        self.title = title
+        self.publication_year = publication_year
+        self.author_id = author_id
 
-
-
+        
