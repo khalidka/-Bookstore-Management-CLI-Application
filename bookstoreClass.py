01320d7b80670f73d308b388029a6e1613965660
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-21 18:05:35 +0300

    feat:Create the 'authors' table

--- bookstoreClass.py
+++ bookstoreClass.py
@@ -2,11 +2,20 @@ class Author:
     def __init__(self, name):
         self.name = name
 
-
+    @classmethod
+    def createTable(cls, cursor, CONN):
+        query = """
+        CREATE TABLE IF NOT EXISTS authors (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT
+        )
+        """
+        cursor.execute(query)
+        CONN.commit()
 class Book:
     def __init__(self, title, publication_year, author_id):
         self.title = title
         self.publication_year = publication_year
         self.author_id = author_id
 
-        
+  
