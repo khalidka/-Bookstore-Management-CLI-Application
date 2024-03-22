ecd8b597475821f076a1f2c14270b7eaa98a1045
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-23 01:46:14 +0300

    feat:Options to manage Authors

--- bookstoreCli.py
+++ bookstoreCli.py
@@ -123,6 +123,36 @@ def manage_books():
         else:
             print("Invalid choice. Please try again.")
 
+def manage_authors():
+    while True:
+        print("\033[92m*\033[0m" * 100)
+        print("\nAuthor Options:")
+        print("\033[94m-\033[0m" * 50)
+        print("1. Add a new author")
+        print("2. View all authors")
+        print("3. Update an author")
+        print("4. Delete an author")
+        print("5. Back to main menu")
+        print("\033[94m-\033[0m" * 50)
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            name = input("Enter the name of the author: ")
+            add_author(name)
+        elif choice == "2":
+            view_authors()
+        elif choice == "3":
+            author_id = int(input("Enter the ID of the author to update: "))
+            name = input("Enter the new name of the author: ")
+            update_author(author_id, name)
+        elif choice == "4":
+            author_id = int(input("Enter the ID of the author to delete: "))
+            delete_author(author_id)
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
 
 def main():
     while True:
